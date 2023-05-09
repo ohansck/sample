d14e3f44d9f2a83be1d423d1f78c2128b4f2b03b
ohansck <ohansck@gmail.com>
2023-05-09 23:16:41 +0100

    mod

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', async (req, res) => {
 });
 app.get('/status', async (req, res) => {
     // connect to MongoDB database
-    mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
+    mongoose.connect('mongodb://app_mongo:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
         .then(() => {
             res.send('MongoDB connected successfully');
         })
